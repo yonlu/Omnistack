96d2c7ab62b3eeddbc93bacb26718a620f8f3b3c
yonlu <lss.emailbox@gmail.com>
2019-06-21 10:12:11 -0400

    fixed coding style

--- node/src/models/Post.js
+++ node/src/models/Post.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new mongoose.Schema(
+  {
     author: String,
     place: String,
     description: String,
     hashtags: String,
     image: String,
     likes: {
-        type: Number,
-        default: 0,
-    }
-}, {
-   timestamps:true,
-});
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model('Post', PostSchema);
